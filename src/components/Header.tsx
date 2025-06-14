2fca8a3e57c25767460bd341d7d60a69d4251fe1
Eric Coleman <miprivatemail@gmail.com>
2025-06-14 12:50:01 -0400

    LOGO SIZE: Increased header logo from h-16 to h-20 (80px) for much more prominent branding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
               <img 
                 src="/CAE_Logo_V3.png" 
                 alt="Car Audio Events" 
-                className="h-16 w-auto"
+                className="h-20 w-auto"
               />
             </Link>
           </div>
