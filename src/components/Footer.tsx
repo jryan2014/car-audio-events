675767c691ac8aba9f09fe72e4eb98a0965a3d7a
Eric Coleman <miprivatemail@gmail.com>
2025-06-14 12:41:53 -0400

    LOGO UPDATE: Updated to CAE_Logo_V3.png in header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -222,7 +222,7 @@ export default function Footer() {
           {/* Brand - Takes 2 columns on large screens */}
           <div className="space-y-4 lg:col-span-2">
             <img 
-              src="/CAE_LogoV2.png" 
+              src="/CAE_Logo_V3.png" 
               alt="Car Audio Events" 
               className="h-16 w-auto"
             />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
           <div className="flex-shrink-0 mr-8">
             <Link to="/" className="flex items-center">
               <img 
-                src="/CAE_LogoV2.png" 
+                src="/CAE_Logo_V3.png" 
                 alt="Car Audio Events" 
                 className="h-14 w-auto"
               />
